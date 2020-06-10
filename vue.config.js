518689cbf73c00eda253e8dabc7922cc06989b99
Dennis Kleber <dennis_kleber@mailbox.org>
2020-06-10 16:57:17 +0200

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
         },
     },
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/widget/payment-backend/'
+        ? '/widget/payment-widget/'
         : '/'
 
 }
