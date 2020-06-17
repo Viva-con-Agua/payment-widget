08a4e3e310da771d3f1124f1ea6e4f4c0bdfad2b
Dennis Kleber <dennis_kleber@mailbox.org>
2020-06-17 15:09:31 +0200

    provide newsletter flag in donation form

--- src/DonationForm.vue
+++ src/DonationForm.vue
@@ -36,8 +36,10 @@
                             :rules="$v.payment.supporter.last_name">
                         </vca-input>
                         </vca-field-row>
-                        <input type="checkbox" v-model="payment.offset.newsletter">
-                        <label for="vehicle1"> Newsletter?</label><br>
+                        <div v-if="reqNewsletter" class="newsletter">
+                            <input type="checkbox" v-model="payment.offset.newsletter">
+                            <label> Newsletter?</label><br>
+                        </div>
                     </vca-field>
                     <Payment v-if="!isCH" v-on:success="success" :payment="payment" :label="getLabel" :country="country" :valid="$v.payment" @notValid="validate"/>
                 </vca-form>
@@ -76,6 +78,10 @@ export default {
             default() {
                 return {}
             }
+        },
+        reqNewsletter: {
+            type: Boolean,
+            default: false
         }
     },
     data() {
