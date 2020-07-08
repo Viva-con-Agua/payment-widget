b149ecb7e8f6c29c60d1d40fbb99dcb39ff51dc1
Dennis Kleber <dennis_kleber@mailbox.org>
2020-07-08 21:55:56 +0200

    changes

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
         },
     },
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/payment-widget/default'
+        ? process.env.VUE_APP_CONTEXT
         : '/'
 
 }
